d793225f73183dcfedbf4db3656d6d7547342403
J-EUM <eumjungmin1@gmail.com>
2022-09-24 02:49:58 +0900

    two sum-82ms

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -14,12 +14,21 @@ class Solution(object):
 # Runtime: 6319 ms, faster than 10.71% of Python online submissions for Two Sum.
 # Memory Usage: 14.3 MB, less than 67.72% of Python online submissions for Two Sum.
 
-        for i, value in enumerate(nums):
-            print(nums)
-            print(f"i={i}, value={value}")
-            remaining = target - value
-            nums[i] = None
-            if remaining in nums:
-                return [i, nums.index(remaining)]
+        # for i, value in enumerate(nums):
+        #     print(nums)
+        #     print(f"i={i}, value={value}")
+        #     remaining = target - value
+        #     nums[i] = None
+        #     if remaining in nums:
+        #         return [i, nums.index(remaining)]
 # Runtime: 1190 ms, faster than 42.52% of Python online submissions for Two Sum.
 # Memory Usage: 14.2 MB, less than 67.72% of Python online submissions for Two Sum.
+
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]   
+            if remaining in seen:
+                return [i, seen[remaining]]           
+            seen[value] = i 
+# Runtime: 82 ms, faster than 68.17% of Python online submissions for Two Sum.
+# Memory Usage: 14.2 MB, less than 85.62% of Python online submissions for Two Sum.
