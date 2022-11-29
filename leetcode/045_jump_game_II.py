f50292c5d5c903a169eee5444f717c4bc4dad36e
J-EUM <eumjungmin1@gmail.com>
2022-11-29 18:21:59 +0900

    jump game 2

--- leetcode/045_jump_game_II.py
+++ leetcode/045_jump_game_II.py
@@ -30,6 +30,32 @@ class Solution(object):
         # nums = [10,1,1,1,1,1,1,1,1,2,3,0,1,4]면 2가나와야되는데 11이 나옴
         # 모르겠다악
 
+        
+
+#다른사람풀이
+# The idea is to maintain two pointers left and right, where left initialy set to be 0 and right set to be nums[0].
+# So points between 0 and nums[0] are the ones you can reach by using just 1 jump.
+# Next, we want to find points I can reach using 2 jumps, so our new left will be set equal to right, and our new right will be set equal to the farest point we can reach by two jumps. which is:
+# right = max(i + nums[i] for i in range(left, right + 1)
+
+# 인덱스 두개 left, right를 둔다 초기값 left: 0, right: nums[0]
+# 0, nums[0]사이 left~right 애들이 1번의 점프를 통해 갈 수 있는 곳들.
+# nums[0]+1부터는 2번의 점프를 통해 갈 수 있다. 갈수있는 최대 인덱스(right)는 현재 left와 right사이값 i + nums[i]중 가장 큰 숫자
+# nums = [2,3,1,1,4]일때
+# 1번점프해서는 0,1,2인덱스까지 갈 수 있고
+# 2번점프해서는 1+3(1+nums[1])=4, 2+1(2+nums[2])=3까지 갈 수 있다.
+# right = max(i + nums[i] for i in range(left, right + 1)
+# 이 작업을 right < len(nums) - 1일때까지 점프 횟수(times)를 1씩 늘리면서 반복한다. 
+# 모든 경우의 수를 다 계산하려고하지 않고 인덱스 두개로 범위를 지정해서 점프1범위, 점프2범위 이렇게 생각하는게 포인트였다..!ㅠ
+        if len(nums) <= 1: return 0
+        l, r = 0, nums[0]
+        times = 1
+        while r < len(nums) - 1:
+            times += 1
+            nxt = max(i + nums[i] for i in range(l, r + 1))
+            l, r = r, nxt
+        return times
+
 
 a = Solution()
 #nums = [2,3,1,1,4] #Output: 2
