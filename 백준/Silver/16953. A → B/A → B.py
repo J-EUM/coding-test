d76751efd09f8a404154ca6782986225df994006
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-09-02 15:57:02 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+cnt = 1
+while b > a:
+    if b % 10 == 1:
+        b //= 10
+        cnt += 1
+    elif b % 2 == 0:
+        b //= 2
+        cnt += 1
+    else:
+        cnt = -1
+        break
+if b == a:
+    print(cnt)
+else:
+    print(-1)
