2f436070c8f262a44e18aaf4463b091963c0086a
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-10-05 01:00:29 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,5 +1,17 @@
 def solution(d, budget):
-    d.sort() 
-    while sum(d) > budget:
-        d.pop()      
-    return len(d)
+    # d.sort() 
+    # while sum(d) > budget:
+    #     d.pop()      
+    # return len(d)
+    
+    d.sort()
+    count = 0
+    
+    for cost in d:
+        if budget - cost >= 0:
+            budget -= cost
+            count += 1
+        else:
+            break
+            
+    return count
