209a2cfc1c974bb149279bffbe8349dc6adfe651
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-10-05 14:40:26 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,17 +1,28 @@
+# def solution(progresses, speeds):
+#     answer = []
+#     days_left = [(99 - progresses[i]) // speeds[i] + 1 for i in range(len(progresses))]
+    
+#     day = days_left[0]
+#     cnt = 1
+    
+#     for d in days_left[1:]:  # 첫 번째 요소는 이미 처리했으므로 슬라이싱
+#         if d <= day:
+#             cnt += 1
+#         else:
+#             answer.append(cnt)
+#             day = d
+#             cnt = 1
+    
+#     answer.append(cnt)
+#     return answer
+
 def solution(progresses, speeds):
-    answer = []
-    days_left = [(99 - progresses[i]) // speeds[i] + 1 for i in range(len(progresses))]
-    
-    day = days_left[0]
-    cnt = 1
-    
-    for d in days_left[1:]:  # 첫 번째 요소는 이미 처리했으므로 슬라이싱
-        if d <= day:
-            cnt += 1
+    Q = []
+    for p, s in zip(progresses, speeds):
+        days_left = (100 - p + s - 1) // s  # 남은 일수 계산 (올림 효과)
+        if not Q or Q[-1][0] < days_left:
+            Q.append([days_left, 1])  # 새로운 배포 그룹 시작
         else:
-            answer.append(cnt)
-            day = d
-            cnt = 1
-    
-    answer.append(cnt)
-    return answer
+            Q[-1][1] += 1  # 같은 배포 그룹에 포함
+    return [q[1] for q in Q]
+
