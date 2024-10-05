61d301af9043761ac5888ebb79189e8d5adaf231
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-10-05 15:26:30 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,28 +1,21 @@
-# def solution(progresses, speeds):
-#     answer = []
-#     days_left = [(99 - progresses[i]) // speeds[i] + 1 for i in range(len(progresses))]
-    
-#     day = days_left[0]
-#     cnt = 1
-    
-#     for d in days_left[1:]:  # 첫 번째 요소는 이미 처리했으므로 슬라이싱
-#         if d <= day:
-#             cnt += 1
-#         else:
-#             answer.append(cnt)
-#             day = d
-#             cnt = 1
-    
-#     answer.append(cnt)
-#     return answer
+from math import ceil
 
 def solution(progresses, speeds):
-    Q = []
-    for p, s in zip(progresses, speeds):
-        days_left = (100 - p + s - 1) // s  # 남은 일수 계산 (올림 효과)
-        if not Q or Q[-1][0] < days_left:
-            Q.append([days_left, 1])  # 새로운 배포 그룹 시작
-        else:
-            Q[-1][1] += 1  # 같은 배포 그룹에 포함
-    return [q[1] for q in Q]
+    answer = []
+    max_duration = ceil((100 - progresses[0]) / speeds[0])
+    count = 0
+    
+    for progress, speed in zip(progresses, speeds):
+        duration = ceil((100 - progress) / speed)
+        # duration = -(-(100 - progress) // speed)
+        if max_duration < duration:
+            answer.append(count)
+            count = 0
+            max_duration = duration
+        count += 1
+    
+    if count > 0:
+        answer.append(count)
+    
+    return answer
 
