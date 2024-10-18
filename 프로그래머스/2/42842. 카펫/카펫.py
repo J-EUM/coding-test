7bb1b441f354eb20cfbb4cc9892f5d2fcb5a3341
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-10-18 11:22:56 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,31 @@
-def solution(brown, yellow):
-    for a in range(yellow, int(yellow ** (1/2) - 1), -1):
-        if yellow % a == 0:
-            b = yellow // a
-            if (a + b + 2) * 2 == brown:
-                return [a + 2, b + 2]
-    return 0
+# def solution(brown, yellow):
+#     for a in range(yellow, int(yellow ** (1/2) - 1), -1):
+#         if yellow % a == 0:
+#             b = yellow // a
+#             if (a + b + 2) * 2 == brown:
+#                 return [a + 2, b + 2]
+#     return 0
+
+
+# def solution(brown, red):
+#     width = (brown + red) // 3
+#     height = 3
+    
+#     while (width - 2) * (height - 2) != red:
+#         width -= 1
+#         height = (brown + red) // width
+    
+#     return [width, height]
+
+
+def solution(brown, red):
+    answer = []
+    brown = (brown - 4)/2
+    for i in range(1, red+1):
+        if red % i == 0:
+            x = red // i
+            y = i
+            if x + y == brown:
+                answer = [x+2, y+2]
+                break
+    return answer
