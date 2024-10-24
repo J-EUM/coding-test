a20e651239cfd51bcd47fae7c8eb8f977b9451ae
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-10-24 19:47:20 +0900

    [level 2] Title: 귤 고르기, Time: 198.67 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -3,8 +3,7 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     c = Counter(tangerine)
-    tangerine.sort()
-    tangerine.sort(key=lambda x: (-c[x]))
+    tangerine.sort(key=lambda x: (-c[x], x))
     
     
     
