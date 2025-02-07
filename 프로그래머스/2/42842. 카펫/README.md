c8f9d050e709459adb60f7190e2513c857ec31df
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-02-07 16:59:10 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.07 ms
+메모리: 10.2 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 11:22:54
+2025년 02월 07일 16:59:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,31 +1,7 @@
-# def solution(brown, yellow):
-#     for a in range(yellow, int(yellow ** (1/2) - 1), -1):
-#         if yellow % a == 0:
-#             b = yellow // a
-#             if (a + b + 2) * 2 == brown:
-#                 return [a + 2, b + 2]
-#     return 0
-
-
-# def solution(brown, red):
-#     width = (brown + red) // 3
-#     height = 3
-    
-#     while (width - 2) * (height - 2) != red:
-#         width -= 1
-#         height = (brown + red) // width
-    
-#     return [width, height]
-
-
-def solution(brown, red):
-    answer = []
-    brown = (brown - 4)/2
-    for i in range(1, red+1):
-        if red % i == 0:
-            x = red // i
-            y = i
-            if x + y == brown:
-                answer = [x+2, y+2]
-                break
-    return answer
+def solution(brown, yellow):
+    for y_height in range(1, int(yellow**(1/2))+1):
+        if yellow % y_height == 0:
+            y_width = yellow // y_height
+            if brown == y_width*2 + y_height*2 + 4:
+                return [y_width+2, y_height+2]
+            
