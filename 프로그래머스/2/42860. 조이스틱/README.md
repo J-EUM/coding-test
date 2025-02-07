b5a7ca3e62e369e15584ba922337b8c0c749dfa2
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-02-07 19:02:34 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 19:02:19
+2025년 02월 07일 19:02:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,22 +1,42 @@
-# https://school.programmers.co.kr/questions/83949
 def solution(name):
-    n = len(name)
-    min_moves = n - 1
-    moves = 0
+    answer = 0
     
-    for i in range(n):
-        diff = ord(name[i]) - ord("A")
-        moves += min(diff, 26 - diff)
+    # A-J: 9, 26-9
+    # A-N: 13, 26-13
+    # JEROEN
+    # 9 4 ...
+    # JAN
+    # 9 0 13
     
-        # 다음번이 A인 경우 연속된 개수 찾기
-        nxt = i + 1
-        while nxt < n and name[nxt] == "A":
-            nxt += 1
-        if nxt - i > 1:
-            # 오른쪽으로 진행 중 왼쪽으로 돌아가면 얼마나 걸리는지
-            distance_to_left_end = i * 2 + n - nxt
-            # 왼쪽으로 진행 중 오른쪽으로 돌아가면 얼마나 걸리는지
-            distance_to_right_end = (n - nxt) * 2 + i
-            min_moves = min(min_moves, distance_to_left_end, distance_to_right_end)
+    # "BBAAAAABBB" : 10
     
-    return moves + min_moves
+    steps = []
+    
+    for ch in name:
+        diff = ord(ch) - ord("A")
+        steps.append(min(diff, 26-diff))
+        
+    if sum(steps) == 0:
+        return 0
+    
+    # AAA  BB  A
+    # 000110
+    # ->: 00011 4칸
+    # <-: 0011  3칸
+    
+    left_count = 0
+    right_count = 0
+    
+    for i in steps[1:]:
+        if i == 0:
+            left_count += 1
+        else:
+            break
+    
+    for i in steps[::-1]:
+        if i == 0:
+            right_count += 1
+        else:
+            break
+    
+    return sum(steps) + len(steps) - max(left_count, right_count) - 1
