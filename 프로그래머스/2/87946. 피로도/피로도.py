9083e493910d1de5321be2002f4c8e373aee0363
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-02 21:03:20 +0900

    [level 2] Title: 피로도, Time: 72.30 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,27 @@
-from itertools import permutations
-
+from collections import deque
+        
 def solution(k, dungeons):
     answer = 0
+    n = len(dungeons)
+    # q = deque([(k, 0, [False] * n)])
+    q = deque([(k, 0, 0)])
     
-    for perm in permutations(dungeons):
-        cnt = 0
-        temp_k = k
+    while q:
+        fatigue, cleared_count, visited = q.popleft()
+        answer = max(answer, cleared_count)
         
-        for i, j in perm:
-            if temp_k >= i:
-                cnt += 1
-                temp_k -= j
-                
-        answer = max(answer, cnt)
-         
+        # for i in range(n):
+        #     if not visited[i] and fatigue >= dungeons[i][0]:
+        #         nxt_visited = visited[:]  # 리스트 복사
+        #         nxt_visited[i] = True
+        #         q.append((fatigue - dungeons[i][1], cleared_count + 1, nxt_visited))
+        for i in range(n):
+            if not (visited & (1 << i)) and fatigue >= dungeons[i][0]:  
+                q.append((
+                    fatigue - dungeons[i][1], 
+                    cleared_count + 1, 
+                    visited | (1 << i)
+                ))
+
+                         
     return answer
