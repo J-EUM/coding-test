17b1b7faf7894ff69d9910b865161a319ed5545b
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-02 22:08:35 +0900

    [level 2] Title: 피로도, Time: 61.57 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -3,25 +3,63 @@ from collections import deque
 def solution(k, dungeons):
     answer = 0
     n = len(dungeons)
-    # q = deque([(k, 0, [False] * n)])
-    q = deque([(k, 0, 0)])
+    q = deque([(k, 0, [False] * n)])
+
     
     while q:
         fatigue, cleared_count, visited = q.popleft()
         answer = max(answer, cleared_count)
         
-        # for i in range(n):
-        #     if not visited[i] and fatigue >= dungeons[i][0]:
-        #         nxt_visited = visited[:]  # 리스트 복사
-        #         nxt_visited[i] = True
-        #         q.append((fatigue - dungeons[i][1], cleared_count + 1, nxt_visited))
         for i in range(n):
-            if not (visited & (1 << i)) and fatigue >= dungeons[i][0]:  
-                q.append((
-                    fatigue - dungeons[i][1], 
-                    cleared_count + 1, 
-                    visited | (1 << i)
-                ))
+            if not visited[i] and fatigue >= dungeons[i][0]:
+                nxt_visited = visited[:]  # 리스트 복사
+                nxt_visited[i] = True
+                q.append((fatigue - dungeons[i][1], cleared_count + 1, nxt_visited))
+                
+    return answer
+
+
+# ㅌㅊ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+        
+# def solution(k, dungeons):
+#     answer = 0
+#     n = len(dungeons)
+#     # q = deque([(k, 0, [False] * n)])
+#     q = deque([(k, 0, 0)])
+    
+#     while q:
+#         fatigue, cleared_count, visited = q.popleft()
+#         answer = max(answer, cleared_count)
+        
+#         # for i in range(n):
+#         #     if not visited[i] and fatigue >= dungeons[i][0]:
+#         #         nxt_visited = visited[:]  # 리스트 복사
+#         #         nxt_visited[i] = True
+#         #         q.append((fatigue - dungeons[i][1], cleared_count + 1, nxt_visited))
+#         for i in range(n):
+#             if not (visited & (1 << i)) and fatigue >= dungeons[i][0]:  
+#                 q.append((
+#                     fatigue - dungeons[i][1], 
+#                     cleared_count + 1, 
+#                     visited | (1 << i)
+#                 ))
 
                          
-    return answer
+#     return answer
