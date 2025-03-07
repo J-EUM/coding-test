fd614c83d940d76bdb9d688bd56543f4653368bd
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-08 00:38:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 549.44 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,3 @@
-import heapq
-
 def solution(operations):
     answer = []
-    
-    for operation in operations:
-        if operation[0] == "I":
-            a, b = operation.split()
-            answer.append(int(b))
-            
-        if operation == "D 1":
-            if answer:
-                v = max(answer)
-                idx = answer.index(v)
-                answer.pop(idx)
-            
-        if operation == "D -1":
-            if answer:
-                v = min(answer)
-                idx = answer.index(v)
-                answer.pop(idx)
-        
-    if answer:
-        return [max(answer), min(answer)]
-    else:
-        return [0, 0]
+    return answer
