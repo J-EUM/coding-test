82c5a14c62e270a57e95d1664356e22a2fc46387
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-04-07 14:38:56 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1821.54 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,5 +1,4 @@
-from collections import deque
-import heapq
+# import heapq 
 
 def solution(users, emoticons):
     # 할인율은 10%, 20%, 30%, 40% 중 하나로 설정됩니다.
@@ -15,22 +14,34 @@ def solution(users, emoticons):
         
     dfs([], len(emoticons))
         
-    
-    heap = []
-    heapq.heapify(heap)
+    result = [0, 0]
+    # heap = []
+    # heapq.heapify(heap)
     for rates in discount:
-        cnt = 0
-        sales = 0
+        cnt = 0  # 가입자수
+        sales = 0  # 매출
         for user_rate, user_price in users:
             user_spent = 0
             for rate, price in zip(rates, emoticons):
                 discounted_price = price * (100 - rate) / 100
                 if rate >= user_rate:
                     user_spent += discounted_price
+            # 일정가격 이상이면 가입
             if user_spent >= user_price:
                 cnt += 1
+            # 아니면 개별구매
             else:
                 sales += user_spent
-        heapq.heappush(heap, [-cnt, -sales])
-    cnt, sales = heapq.heappop(heap)
-    return [-cnt, -sales]
+                
+        # 2. 바로 대소비교
+        if cnt > result[0]:
+            result = [cnt, sales]
+        elif cnt == result[0]:
+            result[1] = max(result[1], sales)
+            
+    return result
+        
+    #     # 1. 힙 사용      
+    #     heapq.heappush(heap, [-cnt, -sales])
+    # cnt, sales = heapq.heappop(heap)
+    # return [-cnt, -sales]
