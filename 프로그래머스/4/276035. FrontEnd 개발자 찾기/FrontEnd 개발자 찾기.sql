e06f8cf95b78670910565227a6a53181dbf91f4e
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-05-18 16:13:10 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 작성해주세요
+SELECT
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM DEVELOPERS d
+WHERE d.SKILL_CODE & (
+    SELECT SUM(CODE) 
+    FROM SKILLCODES 
+    WHERE CATEGORY = 'Front End'
+)
+ORDER BY 1
